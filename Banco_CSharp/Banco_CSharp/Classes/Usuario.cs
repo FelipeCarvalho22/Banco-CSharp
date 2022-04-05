b858f311f0dda4923ca8a6e693855453919b917d
Felipe Silva <felipe-silva22@hotmail.com>
2022-04-05 12:28:44 -0300

    Criando Usuario

---
+++ Banco_CSharp/Banco_CSharp/Classes/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_CSharp.Classes
+{
+    abstract class Usuario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; }
+        public string DataNascimento { get; private set; }
+        public int Idade { get; private set; }
+
+        public Usuario(string cpf, string nascimento)
+        {
+            CPF = cpf;
+
+            DataNascimento = nascimento;
+            Idade = CalculoIdade(this.DataNascimento);
+        }
+
+        private int CalculoIdade(string nascimento)
+        {
+            DateTime dataHora = new DateTime();
+            int anoAtual = dataHora.Year;
+
+            string anoNascimentoString = nascimento.Remove(6);
+            int anoNascimento = Int32.Parse(anoNascimentoString);
+
+            int idade = anoNascimento - anoAtual;
+            return idade;
+        }
+    }
+}
