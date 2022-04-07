b5d72431ed6f9cc106d1f53f9431b0a5b66ed0b8
Felipe Silva <felipe-silva22@hotmail.com>
2022-04-07 18:44:02 -0300

    Criando Classe Conta

---
+++ Banco_CSharp/Banco_CSharp/Classes/Caixa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_CSharp.Classes
+{
+    public class Caixa : Funcionario
+    {
+        public Caixa(string cpf, string nascimento) : base(cpf, nascimento)
+        {
+            //Nenhuma Implementação
+        }
+    }
+}
---
+++ Banco_CSharp/Banco_CSharp/Classes/Conta.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banco_CSharp.Classes;
+
+namespace Banco_CSharp.Classes
+{
+    public abstract class Conta
+    {
+        public Cliente Titular { get; private set; } 
+        public int Agencia { get; private set; }
+        public int Numero { get; private set; }
+
+        private float _saldo = 50;
+        public float Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+        }
+        public static int TotalContasCriadas { get; private set; }
+
+        public Conta(Cliente titular, int numeroAgencia, bool criarPoupanca)
+        {
+            Titular = titular;
+            Agencia = numeroAgencia;
+
+            Numero = 1000 + TotalContasCriadas;
+            TotalContasCriadas++;
+        }
+
+        public bool Saque(float valor)
+        {
+            if (valor > this._saldo || valor < 0)
+            {
+                return false;
+            }
+            this._saldo -= valor;
+            return true;
+        }
+        public bool Deposito(float valor)
+        {
+            if (valor < 0)
+            {
+                return false;
+            }
+            this._saldo += valor;
+            return true;
+        }
+        public bool Transferencia(Conta contaComIdArray, float valor)
+        {
+            if (valor > this.Saldo || valor < 0)
+            {
+                return false;
+            }
+            this.Saque(valor);
+            contaComIdArray.Deposito(valor);
+            return true;
+        }
+    }
+}
