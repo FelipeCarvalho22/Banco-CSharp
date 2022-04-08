f544701772ea7f45dd530e5370fc12549b7e0864
Felipe Silva <felipe-silva22@hotmail.com>
2022-04-07 21:43:28 -0300

    Criando Conta Corrente e Poupança

---
+++ Banco_CSharp/Banco_CSharp/Classes/ContaCorrente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_CSharp.Classes
+{
+    public class ContaCorrente : Conta
+    {
+        private int _digitoVerificador = Conta.TotalContasCriadas;
+        public int DigitoVerificador 
+        {
+            get 
+            { 
+                return _digitoVerificador; 
+            }
+        }
+
+        public ContaCorrente(Cliente titular, int numeroAgencia, bool criarPoupanca) : base(titular, numeroAgencia, criarPoupanca)
+        {
+            if (criarPoupanca == true)
+            {
+                ContaPoupanca contaPoupanca = new ContaPoupanca(titular, numeroAgencia, false, this);
+            }
+        }
+    }
+}
---
+++ Banco_CSharp/Banco_CSharp/Classes/ContaPoupanca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco_CSharp.Classes
+{
+    public class ContaPoupanca : Conta
+    {
+        public int DigitoPoupanca { get; private set; }
+
+        public ContaPoupanca(Cliente titular, int numeroAgencia, bool criarPoupanca, ContaCorrente contaCorrente) : base(titular, numeroAgencia, criarPoupanca)
+        {
+            this.DigitoPoupanca = contaCorrente.DigitoVerificador;
+        }
+    }
+}
