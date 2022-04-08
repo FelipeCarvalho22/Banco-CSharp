eb59a3b4298687443815fd13bd4f9c16a61a8c44
Felipe Silva <felipe-silva22@hotmail.com>
2022-04-08 14:57:06 -0300

    Iniciando inteface do usuario

--- Banco_CSharp/Banco_CSharp/Program.cs
+++ Banco_CSharp/Banco_CSharp/Program.cs
@@ -13,16 +13,33 @@ namespace Banco_CSharp
         static void Main(string[] args)
         {
             var listaClientes = new List<Cliente>();
+            var listaContasCorrentes = new List<ContaCorrente>();
 
-            Gerente.CriarCliente(listaClientes, "399.800.288-07", "12/02/1993");
+            Console.WriteLine("Bem vindo ao Banco C# - Seu banco no prompt de comando");
 
-            Console.WriteLine(listaClientes[0].Id);
-            Console.WriteLine(listaClientes[0].Idade);
+            if (Funcionario.TotalFuncionariosCriados == 0)
+            {
+                Console.WriteLine("Este é o primeiro acesso ao sistema, não temos nenhum funcionario nem cliente, vamos criar seu usuario!");
+                Console.WriteLine();
 
-            DateTime agora = DateTime.Now;
-            Calendar calendario = CultureInfo.InvariantCulture.Calendar;
-            int anoAtual = calendario.GetYear(agora);
-            Console.WriteLine(anoAtual);
+                Console.WriteLine("Por favor digite seu CPF");
+                string cpf = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Agora nos diga sua data de nascimento no formato DD/MM/AAAA");
+                string dataNascimento = Console.ReadLine();
+                var gerenteGeral = new Gerente(cpf, dataNascimento);
+
+                Console.WriteLine("Otimo, nos fale mais sobre você");
+                Console.WriteLine("Por favor digite seu nome:");
+                gerenteGeral.Nome = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine("Vamos confirmar sua ficha");
+                Console.WriteLine($"{gerenteGeral.Nome} você é o Gerente Geral do Banco C#");
+                Console.WriteLine($"Sua idade é de {gerenteGeral.Idade} e seu CPF é o numero {gerenteGeral.CPF}");
+                Console.WriteLine($"Anote seu numero de matricula: {gerenteGeral.Matricula}");
+
+            }
 
             Console.ReadLine();
         }
