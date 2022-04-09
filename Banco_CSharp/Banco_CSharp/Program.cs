16301fc7389eaa3a3369dc11975a5a338b269527
Felipe Silva <felipe-silva22@hotmail.com>
2022-04-09 17:01:29 -0300

    Continuando a interface

--- Banco_CSharp/Banco_CSharp/Program.cs
+++ Banco_CSharp/Banco_CSharp/Program.cs
@@ -14,8 +14,12 @@ namespace Banco_CSharp
         {
             var listaClientes = new List<Cliente>();
             var listaContasCorrentes = new List<ContaCorrente>();
+            var listaGerentes = new List<Gerente>();
+            var listaCaixa = new List<Caixa>();
 
             Console.WriteLine("Bem vindo ao Banco C# - Seu banco no prompt de comando");
+
+            Inicio:
 
             if (Funcionario.TotalFuncionariosCriados == 0)
             {
@@ -38,9 +42,53 @@ namespace Banco_CSharp
                 Console.WriteLine($"{gerenteGeral.Nome} você é o Gerente Geral do Banco C#");
                 Console.WriteLine($"Sua idade é de {gerenteGeral.Idade} e seu CPF é o numero {gerenteGeral.CPF}");
                 Console.WriteLine($"Anote seu numero de matricula: {gerenteGeral.Matricula}");
+                Console.WriteLine("Aperte qualquer tecla para continuar!");
+                Console.ReadLine();
+                Console.Clear();
 
+                Console.WriteLine();
+                Console.WriteLine("Dando continuidade, você precissa de no minimo mais um funcinario para ser o caixa, vamos adiciona-lo ao sistema!");
+                Console.WriteLine("Por favor digite o CPF do colaborador");
+                string cpfCaixa = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Agora nos diga a data de nascimento no formato DD/MM/AAAA");
+                string dataNascimentoCaixa = Console.ReadLine();
+                listaCaixa.Add(new Caixa(cpfCaixa, dataNascimentoCaixa));
+                Console.WriteLine("Por favor, nos informe o nome deste colaborador:");
+                listaCaixa[0].Nome = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine("Vamos confirmar a ficha do colaborador Caixa!");
+                Console.WriteLine($"{listaCaixa[0].Nome} você é o primeiro colaborador do Banco C#");
+                Console.WriteLine($"Sua idade é de {listaCaixa[0].Idade} e seu CPF é o numero {listaCaixa[0].CPF}");
+                Console.WriteLine($"Por favor anote a sua matricula: {listaCaixa[0].Matricula}");
+
+                Console.WriteLine();
+                Console.WriteLine("Obrigado, reinicie o sistema para finalizar, aperte qualquer tecla");
+                Console.ReadLine();
+                Console.Clear();
+
+                goto Inicio;
+            }
+            else
+            {
+                Console.WriteLine("Por favor nos informe quem esta acessando: 1-Gerente, 2-Caixa, 3-Cliente ou qualquer outro numero para sair.");
+                int opcao = Int32.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Bem vindo ao sistema da gerencia!");
+                        goto Fim;
+                    case 2:
+                        Console.WriteLine("Bem vindo ao sistema do caixa!");
+                        goto Fim;
+                    case 3:
+                        Console.WriteLine("Bem vindo a área do cliente!");
+                        goto Fim;
+                }
             }
 
+            Fim:
             Console.ReadLine();
         }
     }
